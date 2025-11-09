12e12ca4af9ad3d7d9e1b393bafe0736da07c756
ExtremeXT <extremextdev@gmail.com>
2025-11-09 16:28:07 +0200

    r0s: Set FOD Sensor Location property
    Change-Id: I39e020fe444793c7789b500b9b8acc72b9d0f5c2
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,5 +12,8 @@ DEVICE_PATH := device/samsung/r0s
 # Display
 TARGET_SCREEN_DENSITY := 480
 
+# Properties
+TARGET_VENDOR_PROP += device/samsung/r0s/configs/props/vendor.prop
+
 # inherit from the proprietary version
 -include vendor/samsung/r0s/BoardConfigVendor.mk
