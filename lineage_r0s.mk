ExtremeXT/android_device_samsung_g0s

r0s: Define device identifiers
Change-Id: Ifa165e3391c267abc40f4f98e0dc59d5d2d5f106
Signed-off-by: ExtremeXT <extremextdev@gmail.com>

lineage_r0s.mk
@@ -6,3 +6,19 @@
 
 # Inherit from r0s device
 $(call inherit-product, device/samsung/r0s/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := r0s
+PRODUCT_NAME := lineage_r0s
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-S901B
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+# Use the latest approved GMS identifiers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DeviceProduct=r0sxxx \
+    SystemName=r0sxxx \
+    BuildFingerprint=samsung/r0sxxx/r0s:12/SP1A.210812.016/S901BXXSIFYI3:user/release-keys \
+    BuildDesc="r0sxxx-user 12 SP1A.210812.016 S901BXXSIFYI3 release-keys"

ExtremeXT/android_device_samsung_r0s

lineage_r0s.mk
@@ -6,3 +6,19 @@
 
 # Inherit from r0s device
 $(call inherit-product, device/samsung/r0s/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := r0s
+PRODUCT_NAME := lineage_r0s
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-S901B
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+# Use the latest approved GMS identifiers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DeviceProduct=r0sxxx \
+    SystemName=r0sxxx \
+    BuildFingerprint=samsung/r0sxxx/r0s:12/SP1A.210812.016/S901BXXSKGYK5:user/release-keys \
+    BuildDesc="r0sxxx-user 12 SP1A.210812.016 S901BXXSKGYK5 release-keys"